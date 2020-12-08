840a88fab7256e455ada85a532f36e64d42b2ea8
dipesh <kcdipesh429@gmail.com>
2020-12-08 20:07:26 +0545

    feat:setup routes with cors

---
+++ router.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"golang-gin-gorm-2fa/controller"
+	"net/http"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+//SetupRoutes : gin router
+func SetupRoutes() {
+	httpRouter := gin.Default()
+
+	httpRouter.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+	}))
+
+	httpRouter.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": " API Up and Running"})
+	})
+
+	httpRouter.GET("register", controller.Register)
+	httpRouter.GET("validate", controller.Validate)
+
+	httpRouter.Run(":5000")
+
+}
